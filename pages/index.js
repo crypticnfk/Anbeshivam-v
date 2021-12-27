0a81f872edd0e45265bb8679f8dfe91da453ed9b
crypticnfk <nfk15011@protonmail.com>
2021-12-27 15:51:40 +0530

    Updated page

--- pages/index.js
+++ pages/index.js
@@ -5,41 +5,28 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>AnbeShivam</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://anbe-shivam-metis.vercel.app">AnbeShivam!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Get started
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://anbe-shivam-metis.vercel.app" target="_blank" className={styles.card}>
+            <h3>Visit the DApp &rarr;</h3>
+            <p>Find in-depth information about AnbeShivam</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with 4everland.
-            </p>
+          <a href="https://anbe-shivam-metis.vercel.app/nfts" className={styles.card}>
+            <h3>NFTs &rarr;</h3>
+            <p>View your AnbeShivam NFTs</p>
           </a>
         </div>
       </main>
